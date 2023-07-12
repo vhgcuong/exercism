ef46633b60faac57dadc31f7216c084a515d37f8
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-12 16:38:44 +0700

    Prime factors

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod nth_prime;
 pub mod armstrong;
+pub mod prime_factors;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
-use armstrong::is_armstrong_number;
-use nth_prime::nth_prime::nth;
+use crate::prime_factors::factors;
+// use armstrong::is_armstrong_number;
+// use nth_prime::nth_prime::nth;
 
-pub mod armstrong;
-pub mod nth_prime;
+// pub mod armstrong;
+// pub mod nth_prime;
+
+pub mod prime_factors;
 
 fn main() {
     let mut input = String::new();
@@ -13,15 +16,17 @@ fn main() {
         .read_line(&mut input)
         .expect("Không thể đọc dữ liệu từ dòng lệnh");
 
-    let input = match input.trim().parse::<i32>() {
+    let input = match input.trim().parse::<u64>() {
         Ok(number) => number,
         Err(_) => {
-            println!("Không thể chuyển đổi thành i32");
+            println!("Không thể chuyển đổi thành");
             return; // Thoát khỏi chương trình nếu không thể chuyển đổi
         }
     };
 
     // is_armstrong_number(input as u32);
 
-   println!("{:?}", nth(input as u32));
+   // nth(input as u32);
+
+    println!("{:?}", factors(input as u64));
 }
