9ac1d28dac2931fc7d1f974d645cb3161e4af4ac
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-13 10:32:18 +0700

    Sum of multiples

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 use std::io;
-use crate::prime_factors::factors;
+// use crate::prime_factors::factors;
 // use armstrong::is_armstrong_number;
 // use nth_prime::nth_prime::nth;
 
 // pub mod armstrong;
 // pub mod nth_prime;
+// pub mod prime_factors;
 
-pub mod prime_factors;
+pub mod sum_of_multiples;
 
 fn main() {
-    let mut input = String::new();
-
-    println!("Nhập một số nguyên: ");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Không thể đọc dữ liệu từ dòng lệnh");
-
-    let input = match input.trim().parse::<u64>() {
-        Ok(number) => number,
-        Err(_) => {
-            println!("Không thể chuyển đổi thành");
-            return; // Thoát khỏi chương trình nếu không thể chuyển đổi
-        }
-    };
+    // let mut input = String::new();
+    //
+    // println!("Nhập một số nguyên: ");
+    // io::stdin()
+    //     .read_line(&mut input)
+    //     .expect("Không thể đọc dữ liệu từ dòng lệnh");
+    //
+    // let input = match input.trim().parse::<u64>() {
+    //     Ok(number) => number,
+    //     Err(_) => {
+    //         println!("Không thể chuyển đổi thành");
+    //         return; // Thoát khỏi chương trình nếu không thể chuyển đổi
+    //     }
+    // };
 
     // is_armstrong_number(input as u32);
 
    // nth(input as u32);
 
-    println!("{:?}", factors(input as u64));
+    // println!("{:?}", factors(input as u64));
+
+    println!("{}", sum_of_multiples::sum_of_multiples(4, &[3, 0]));
 }
