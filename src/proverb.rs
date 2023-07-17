141084c5088fec1253ef8b4d2e936554520b9a0b
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-17 15:13:49 +0700

    Update Proverb

--- src/proverb.rs
+++ src/proverb.rs
@@ -1,19 +1,29 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut expected = String::new();
+    // let mut expected = String::new();
+    //
+    // if list.is_empty() {
+    //     return expected;
+    // }
+    //
+    // for i in 0..list.len() - 1 {
+    //     if (i+1) < list.len() {
+    //         expected.push_str(&format!("For want of a {} the {} was lost.\n", list[i], list[i+1]))
+    //     }
+    // }
+    //
+    // expected.push_str(&format!("And all for the want of a {}.", list[0]));
+    //
+    // return expected;
 
-    if list.is_empty() {
-        return expected;
+    match list.first() {
+        None => String::new(),
+        Some(word) => list.windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
-
-    for i in 0..list.len() - 1 {
-        if (i+1) < list.len() {
-            expected.push_str(&format!("For want of a {} the {} was lost.\n", list[i], list[i+1]))
-        }
-    }
-
-    expected.push_str(&format!("And all for the want of a {}.", list[0]));
-
-    return expected;
 }
 
 #[cfg(test)]
