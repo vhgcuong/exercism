04dd285b475b5be80f8ec82fe08d3e3edf02d500
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-17 15:30:50 +0700

    Base reverse ...

--- src/reverse.rs
+++ src/reverse.rs
@@ -1,61 +1,65 @@
 pub fn reverse(input: &str) -> String {
     unimplemented!("Write a function to reverse {input}");
-
 }
 
-#[cfg(test)]
-mod reverse_tests {
-    use super::*;
+#[test]
+///
+fn process_reverse_case(input: &str, expected: &str) {
+    assert_eq!(&reverse(input), expected)
+}
 
-    fn process_reverse_case(input: &str, expected: &str) {
-        assert_eq!(&reverse(input), expected)
-    }
-    #[test]
-    /// empty string
-    fn test_an_empty_string() {
-        process_reverse_case("", "");
-    }
-    #[test]
-    #[ignore]
-    /// a word
-    fn test_a_word() {
-        process_reverse_case("robot", "tobor");
-    }
-    #[test]
-    #[ignore]
-    /// a capitalized word
-    fn test_a_capitalized_word() {
-        process_reverse_case("Ramen", "nemaR");
-    }
-    #[test]
-    #[ignore]
-    /// a sentence with punctuation
-    fn test_a_sentence_with_punctuation() {
-        process_reverse_case("I'm hungry!", "!yrgnuh m'I");
-    }
-    #[test]
-    #[ignore]
-    /// a palindrome
-    fn test_a_palindrome() {
-        process_reverse_case("racecar", "racecar");
-    }
-    #[test]
-    #[ignore]
-    /// an even-sized word
-    fn test_an_even_sized_word() {
-        process_reverse_case("drawer", "reward");
-    }
-    #[test]
-    #[ignore]
-    /// wide characters
-    fn test_wide_characters() {
-        process_reverse_case("子猫", "猫子");
-    }
-    #[test]
-    #[ignore]
-    #[cfg(feature = "grapheme")]
-    /// grapheme clusters
-    fn test_grapheme_clusters() {
-        process_reverse_case("uüu", "uüu");
-    }
+#[test]
+/// empty string
+fn test_an_empty_string() {
+    process_reverse_case("", "");
+}
+
+#[test]
+#[ignore]
+/// a word
+fn test_a_word() {
+    process_reverse_case("robot", "tobor");
+}
+
+#[test]
+#[ignore]
+/// a capitalized word
+fn test_a_capitalized_word() {
+    process_reverse_case("Ramen", "nemaR");
+}
+
+#[test]
+#[ignore]
+/// a sentence with punctuation
+fn test_a_sentence_with_punctuation() {
+    process_reverse_case("I'm hungry!", "!yrgnuh m'I");
+}
+
+#[test]
+#[ignore]
+/// a palindrome
+fn test_a_palindrome() {
+    process_reverse_case("racecar", "racecar");
+}
+
+#[test]
+#[ignore]
+/// an even-sized word
+fn test_an_even_sized_word() {
+    process_reverse_case("drawer", "reward");
+}
+
+#[test]
+#[ignore]
+/// wide characters
+fn test_wide_characters() {
+    process_reverse_case("子猫", "猫子");
+}
+
+#[test]
+#[ignore]
+#[cfg(feature = "grapheme")]
+/// grapheme clusters
+fn test_grapheme_clusters() {
+    process_reverse_case("uüu", "uüu");
 }
