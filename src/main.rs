3434a84f2e442034899610e03d8eafa58130c739
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-18 17:32:45 +0700

    Reverse

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::io;
 // use crate::prime_factors::factors;
 // use armstrong::is_armstrong_number;
 // use nth_prime::nth_prime::nth;
-use raindrops::raindrops;
+// use raindrops::raindrops;
+// use reverse::reverse;
 
 // pub mod armstrong;
 // pub mod nth_prime;
 // pub mod prime_factors;
 // pub mod sum_of_multiples;
-pub mod raindrops;
+// pub mod raindrops;
+// pub mod reverse;
 
 fn main() {
     let mut input = String::new();
@@ -37,5 +39,5 @@ fn main() {
 
     // print!("{}", difference(input as u64));
 
-    println!("{}", raindrops(input as u32))
+    // println!("{}", raindrops(input as u32))
 }
--- src/reverse.rs
+++ src/reverse.rs
@@ -1,65 +1,77 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {input}");
+    let mut str_reverse = String::new();
+    if input.is_empty() {
+        return str_reverse;
+    }
+
+    for item in input.chars().rev() {
+        str_reverse.push(item);
+    }
+
+    str_reverse
 }
 
-#[test]
-///
-fn process_reverse_case(input: &str, expected: &str) {
-    assert_eq!(&reverse(input), expected)
-}
+#[cfg(test)]
+mod reverse_tests {
+    use super::*;
 
-#[test]
-/// empty string
-fn test_an_empty_string() {
-    process_reverse_case("", "");
-}
+    fn process_reverse_case(input: &str, expected: &str) {
+        assert_eq!(&reverse(input), expected)
+    }
 
-#[test]
-#[ignore]
-/// a word
-fn test_a_word() {
-    process_reverse_case("robot", "tobor");
-}
+    #[test]
+    /// empty string
+    fn test_an_empty_string() {
+        process_reverse_case("", "");
+    }
 
-#[test]
-#[ignore]
-/// a capitalized word
-fn test_a_capitalized_word() {
-    process_reverse_case("Ramen", "nemaR");
-}
+    #[test]
+    #[ignore]
+    /// a word
+    fn test_a_word() {
+        process_reverse_case("robot", "tobor");
+    }
 
-#[test]
-#[ignore]
-/// a sentence with punctuation
-fn test_a_sentence_with_punctuation() {
-    process_reverse_case("I'm hungry!", "!yrgnuh m'I");
-}
+    #[test]
+    #[ignore]
+    /// a capitalized word
+    fn test_a_capitalized_word() {
+        process_reverse_case("Ramen", "nemaR");
+    }
 
-#[test]
-#[ignore]
-/// a palindrome
-fn test_a_palindrome() {
-    process_reverse_case("racecar", "racecar");
-}
+    #[test]
+    #[ignore]
+    /// a sentence with punctuation
+    fn test_a_sentence_with_punctuation() {
+        process_reverse_case("I'm hungry!", "!yrgnuh m'I");
+    }
 
-#[test]
-#[ignore]
-/// an even-sized word
-fn test_an_even_sized_word() {
-    process_reverse_case("drawer", "reward");
-}
+    #[test]
+    #[ignore]
+    /// a palindrome
+    fn test_a_palindrome() {
+        process_reverse_case("racecar", "racecar");
+    }
 
-#[test]
-#[ignore]
-/// wide characters
-fn test_wide_characters() {
-    process_reverse_case("子猫", "猫子");
-}
+    #[test]
+    #[ignore]
+    /// an even-sized word
+    fn test_an_even_sized_word() {
+        process_reverse_case("drawer", "reward");
+    }
 
-#[test]
-#[ignore]
-#[cfg(feature = "grapheme")]
-/// grapheme clusters
-fn test_grapheme_clusters() {
-    process_reverse_case("uüu", "uüu");
+    #[test]
+    #[ignore]
+    /// wide characters
+    fn test_wide_characters() {
+        process_reverse_case("子猫", "猫子");
+    }
+
+    #[test]
+    #[ignore]
+    #[cfg(feature = "grapheme")]
+    /// grapheme clusters
+    fn test_grapheme_clusters() {
+        process_reverse_case("uüu", "uüu");
+    }
 }
