957c8283a2d35f906e8cfd3e55b8215cdb8ef559
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-21 15:05:19 +0700

    Update series

--- src/series.rs
+++ src/series.rs
@@ -1,23 +1,33 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut series: Vec<String> = Vec::new();
-    let numbers: Vec<char> = digits.chars().collect();
+    // let mut series: Vec<String> = Vec::new();
+    // let numbers: Vec<char> = digits.chars().collect();
+    //
+    // if numbers.len() < len {
+    //     return series;
+    // }
+    //
+    // for index in 0..=numbers.len() {
+    //     if len == 0 {
+    //         series.push("".to_string());
+    //     } else {
+    //         if (index + len) > numbers.len() {
+    //             break;
+    //         }
+    //         series.push(numbers[index..(index + len)].iter().collect());
+    //     }
+    // }
+    //
+    // series
 
-    if numbers.len() < len {
-        return series;
+    if len == 0 {
+        return vec!["".to_string(); digits.len() + 1];
     }
-
-    for index in 0..=numbers.len() {
-        if len == 0 {
-            series.push("".to_string());
-        } else {
-            if (index + len) > numbers.len() {
-                break;
-            }
-            series.push(numbers[index..(index + len)].iter().collect());
-        }
-    }
-
-    series
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|c| c.into_iter().collect::<String>())
+        .collect()
 }
 
 #[cfg(test)]
