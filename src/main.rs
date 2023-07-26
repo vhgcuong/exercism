e6a48a71715c135b9c6b56b512cd1e08aac8371e
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-26 15:47:17 +0700

    Comment main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+// use std::io;
 
 // use crate::prime_factors::factors;
 // use armstrong::is_armstrong_number;
@@ -14,20 +14,20 @@ use std::io;
 // pub mod reverse;
 
 fn main() {
-    let mut input = String::new();
-
-    println!("Nhập một số nguyên: ");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Không thể đọc dữ liệu từ dòng lệnh");
-
-    let input = match input.trim().parse::<u64>() {
-        Ok(number) => number,
-        Err(_) => {
-            println!("Không thể chuyển đổi thành");
-            return; // Thoát khỏi chương trình nếu không thể chuyển đổi
-        }
-    };
+    // let mut input = String::new();
+    //
+    // println!("Nhập một số nguyên: ");
+    // io::stdin()
+    //     .read_line(&mut input)
+    //     .expect("Không thể đọc dữ liệu từ dòng lệnh");
+    //
+    // let input = match input.trim().parse::<u64>() {
+    //     Ok(number) => number,
+    //     Err(_) => {
+    //         println!("Không thể chuyển đổi thành");
+    //         return; // Thoát khỏi chương trình nếu không thể chuyển đổi
+    //     }
+    // };
 
     // is_armstrong_number(input as u32);
 
