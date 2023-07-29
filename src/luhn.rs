39b102cb9ca82b6b3bf9508f6d32d9dcee287e52
Yotoko <vhgcuong95@gmail.com>
2023-07-29 21:54:32 +0700

    Update luhn

--- src/luhn.rs
+++ src/luhn.rs
@@ -1,31 +1,15 @@
 /// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
+pub fn is_valid(s: &str) -> bool {
     let mut sum = 0;
-    let numbers: Vec<char> = code.chars().rev().collect();
-    let mut index = 0;
-
-    for number in numbers {
-        if number.is_whitespace() {
-            continue;
-        }
-
-        match number.to_digit(10) {
-            Some(value) => {
-                if index % 2 == 0 {
-                    sum += value;
-                } else {
-                    if value > 4 {
-                        sum += value * 2 - 9;
-                    } else {
-                        sum += value * 2;
-                    }
-                }
-
-                index += 1;
-            },
-            None => return false
+    let mut len = 0;
+    for (i, c) in s.chars().rev().filter(|&c| c != ' ').enumerate() {
+        len += 1;
+        match (i % 2, c.to_digit(10)) {
+            (1, Some(x)) if x > 4 => sum += x * 2 - 9,
+            (1, Some(x)) => sum += x * 2,
+            (0, Some(x)) => sum += x,
+            (_, _) => return false
         }
     }
-
-    index > 1 && sum % 10 == 0
+    (len > 1) && (sum % 10 == 0)
 }
