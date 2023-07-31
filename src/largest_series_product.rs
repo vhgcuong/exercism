61ef7a170f52e7bb9ba9f9beeb9be5f9acb8bbcb
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-07-31 10:49:02 +0700

    update largest series product

--- src/largest_series_product.rs
+++ src/largest_series_product.rs
@@ -5,6 +5,24 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    // if span > string_digits.len() {
+    //     return Err(Error::SpanTooLong);
+    // }
+    // let digits: Vec<u64> = string_digits
+    //     .chars()
+    //     .map(|c| {
+    //         c.to_digit(10)
+    //             .map(|d| d as u64)
+    //             .ok_or(Error::InvalidDigit(c))
+    //     })
+    //     .collect::<Result<Vec<u64>, Error>>()?;
+    // let largest_product = digits
+    //     .windows(span)
+    //     .map(|a| a.iter().product())
+    //     .max()
+    //     .unwrap();
+    // Ok(largest_product)
+
     match span {
         0 => Ok(1),
         _ => string_digits
