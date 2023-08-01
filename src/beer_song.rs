d387bd97eb253282a25b62fd5e1a11b8489cad63
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-08-01 13:19:05 +0700

    Update beer song

--- src/beer_song.rs
+++ src/beer_song.rs
@@ -1,32 +1,21 @@
-pub fn verse(n: u32) -> String {
-    if n < 1 {
-        return String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+const VERSES: [&'static str; 3] = [
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+];
+pub fn verse(n: u16) -> String {
+    match n {
+        0 => VERSES[0].to_string(),
+        1 => VERSES[1].to_string(),
+        2 => VERSES[2].to_string(),
+        _ => format!("{0} bottles of beer on the wall, {0} bottles of beer.\n\
+                   Take one down and pass it around, {1} bottles of beer \
+                   on the wall.\n", n, n-1)
     }
-
-    if n == 1 {
-        return String::from(format!("{0} bottle of beer on the wall, {0} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n));
-    }
-
-    if n - 1 == 1 {
-        return String::from(format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottle of beer on the wall.\n", n, n-1))
-    }
-
-    return String::from(format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n", n, n-1))
 }
-
-pub fn sing(start: u32, end: u32) -> String {
-    let mut text = String::new();
-    let mut index = start;
-    loop {
-        text.push_str(&verse(index));
-
-        if index > end {
-            text.push_str("\n");
-            index -= 1;
-        } else {
-            break;
-        }
-    }
-
-    text
+pub fn sing(from: u16, to: u16) -> String {
+    (to .. from + 1).rev()
+        .map(|x| verse(x))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
