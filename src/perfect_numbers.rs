3ac71b24a99b9102c291af7355d0110e2b22a1eb
Yotoko <vhgcuong95@gmail.com>
2023-08-11 22:40:58 +0700

    Update Perfect numbers

--- src/perfect_numbers.rs
+++ src/perfect_numbers.rs
@@ -6,17 +6,24 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    match num {
-        0 => None,
-        1 => Some(Classification::Deficient),
-        _ => {
-            let sum_aliquot: u64 = (1..=num/2).filter(|&x| num % x == 0).sum();
+    // match num {
+    //     0 => None,
+    //     1 => Some(Classification::Deficient),
+    //     _ => {
+    //         let sum_aliquot: u64 = (1..=num/2).filter(|&x| num % x == 0).sum();
+    //
+    //         Some(match sum_aliquot.cmp(&num) {
+    //             std::cmp::Ordering::Greater => Classification::Abundant,
+    //             std::cmp::Ordering::Less => Classification::Deficient,
+    //             _ => Classification::Perfect,
+    //         })
+    //     }
+    // }
 
-            Some(match sum_aliquot.cmp(&num) {
-                std::cmp::Ordering::Greater => Classification::Abundant,
-                std::cmp::Ordering::Less => Classification::Deficient,
-                _ => Classification::Perfect,
-            })
-        }
+    match (1..=num / 2).filter(|i| num % i == 0).sum::<u64>() {
+        _ if num == 0 => None,
+        x if x > num => Some(Classification::Abundant),
+        x if x < num => Some(Classification::Deficient),
+        _ => Some(Classification::Perfect),
     }
 }
