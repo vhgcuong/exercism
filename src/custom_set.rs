b71596c51e163d40f3c25d265256005b730b6f3a
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-08-16 15:48:00 +0700

    update custom set

--- src/custom_set.rs
+++ src/custom_set.rs
@@ -45,6 +45,65 @@ impl<T> CustomSet<T>
     }
 }
 
+// #[derive(Debug, PartialEq)]
+// pub struct CustomSet<T> {
+//     set: Vec<T>,
+// }
+// impl<T> CustomSet<T>
+// where
+//     // dedup relies on PartialEq. Tests pass without it, but it should be there
+//     T: PartialEq + Clone + Ord,
+// {
+//     pub fn new(input: &[T]) -> Self {
+//         let mut vec: Vec<T> = input.into_iter().cloned().collect();
+//         vec.sort_unstable();
+//         vec.dedup();
+//         Self { set: vec }
+//     }
+//     pub fn contains(&self, element: &T) -> bool {
+//         self.set.contains(element)
+//     }
+//     pub fn add(&mut self, element: T) {
+//         self.set.push(element);
+//         self.set.sort_unstable();
+//         self.set.dedup();
+//     }
+//     pub fn is_subset(&self, other: &Self) -> bool {
+//         self.set.iter().all(|item| other.contains(item))
+//     }
+//     pub fn is_empty(&self) -> bool {
+//         self.set.is_empty()
+//     }
+//     pub fn is_disjoint(&self, other: &Self) -> bool {
+//         self.set.iter().all(|item| !other.contains(item))
+//     }
+//     pub fn intersection(&self, other: &Self) -> Self {
+//         let vec = self
+//             .set
+//             .iter()
+//             .filter(|item| other.contains(item))
+//             .cloned()
+//             .collect();
+//         Self { set: vec }
+//     }
+//     pub fn difference(&self, other: &Self) -> Self {
+//         let vec = self
+//             .set
+//             .iter()
+//             .filter(|item| !other.contains(item))
+//             .cloned()
+//             .collect();
+//         Self { set: vec }
+//     }
+//     pub fn union(&self, other: &Self) -> Self {
+//         let mut vec = self.set.clone();
+//         vec.extend_from_slice(&other.set);
+//         vec.sort_unstable();
+//         vec.dedup();
+//         Self { set: vec }
+//     }
+// }
+
 // #[derive(Debug, PartialEq, Eq)]
 // pub struct CustomSet<T> {
 //     values: Vec<T>,
