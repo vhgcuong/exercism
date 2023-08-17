772f529e53640fefceb7dc06486e3e2cdaea6150
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2023-08-17 15:39:14 +0700

    Update luhn trait

--- src/luhn_trait.rs
+++ src/luhn_trait.rs
@@ -1,107 +1,26 @@
-pub trait Luhn {
-    fn valid_luhn(&self) -> bool;
-}
-
-/// Here is the example of how to implement custom Luhn trait
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+pub trait Luhn: ToString {
     fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
+        let code = self.to_string();
+        if code.trim().len() <= 1 {
+            return false;
+        }
+        if !code.chars().all(|ch| ch.is_digit(10) || ch.is_whitespace()) {
+            return false;
+        }
+        let numbers_sum: u32 = code
+            .chars()
             .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
+            .filter_map(|ch| ch.to_digit(10))
+            .enumerate()
+            .map(|(i, n)|
+                match i % 2 {
+                    0 => n,
+                    _ if n == 9 => n,
+                    _ => (n * 2) % 9,
+                }
+            )
+            .sum();
+        numbers_sum % 10 == 0
     }
 }
-
-impl Luhn for String {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
-
-impl Luhn for u8 {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
-
-impl Luhn for u16 {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
-
-impl Luhn for u32 {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
-
-
-impl Luhn for u64 {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
-
-impl Luhn for usize {
-    fn valid_luhn(&self) -> bool {
-        self.to_string().chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(sum, count), val| {
-                val.to_digit(10)
-                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
-                    .map(|num| if num > 9 { num - 9 } else { num })
-                    .map(|num| (num + sum, count + 1))
-            }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
-    }
-}
+impl<T: ToString> Luhn for T {}
