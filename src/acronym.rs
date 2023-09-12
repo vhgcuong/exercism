c7572c090e2ed9bffe480b28a0fde5c329e5e477
Yotoko <44945530+vhgcuong@users.noreply.github.com>
2023-09-12 10:40:55 +0700

    Update code acronym

--- src/acronym.rs
+++ src/acronym.rs
@@ -1,37 +1,13 @@
 pub fn abbreviate(phrase: &str) -> String {
-    let texts: Vec<&str> = phrase.split(|c: char| c.is_ascii_whitespace() || c == '_' || c == '-').collect();
-    let mut result = String::from("");
-
-    for text in texts {
-        //  Lấy ký tự đầu tiên
-        result.push_str(&text
-            .chars()
-            .take(1)
-            .collect::<String>()
-        );
-
-        // Lấy ký tự viết hoa trong từ
-        // HyperText -> T
-        // GNU -> G
-        result.push_str(&text
-            .chars()
-            .skip_while(|c| c.is_ascii_uppercase())
-            .filter(|c| c.is_ascii_uppercase())
-            .collect::<String>()
-        );
-    }
-
-    result.to_uppercase()
-
-    // phrase
-    //     .split(|c: char| c.is_ascii_whitespace() || c == '_' || c == '-')
-    //     .flat_map(|word| {
-    //         word.chars().take(1).chain(
-    //             word.chars()
-    //                 .skip_while(|c| c.is_ascii_uppercase())
-    //                 .filter(|c| c.is_ascii_uppercase()),
-    //         )
-    //     })
-    //     .collect::<String>()
-    //     .to_ascii_uppercase()
+    phrase
+        .split(|c: char| c.is_ascii_whitespace() || c == '_' || c == '-')
+        .flat_map(|word| {
+            word.chars().take(1).chain(
+                word.chars()
+                    .skip_while(|c| c.is_ascii_uppercase())
+                    .filter(|c| c.is_ascii_uppercase()),
+            )
+        })
+        .collect::<String>()
+        .to_ascii_uppercase()
 }
