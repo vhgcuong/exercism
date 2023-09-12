b963c107d5f7003def8e642ce9c0ffb40f4720c0
Yotoko <44945530+vhgcuong@users.noreply.github.com>
2023-09-12 17:31:03 +0700

    Alphametics

--- src/alphametics.rs
+++ src/alphametics.rs
@@ -1,5 +1,50 @@
-use std::collections::HashMap;
+use std::iter::FromIterator;
+use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    unimplemented!("Solve the alphametic {input:?}")
+    let mut iter = input.clone().split("==");
+
+    let sum: Vec<&str> = iter.next().unwrap().split("+").map(|x| x.trim()).collect();
+    let answer: &str = iter.next().unwrap().trim();
+
+    // sum cannot be shorter than addends
+    if sum.iter().any(|x| x.len() > answer.len()) {
+        println!("1");
+        return None;
+    }
+
+    let alphabet: HashSet<char> = HashSet::from_iter(input.chars().filter(char::is_ascii_alphabetic));
+    let perms = (0..=9).permutations(alphabet.len());
+
+    for perm in perms {
+        let value_map: HashMap<char, u8> = HashMap::from_iter(alphabet.iter().cloned().zip(perm.iter().cloned()));
+
+        // no leading zeroes
+        if value_map[&answer.chars().nth(0).unwrap()] == 0 {
+            continue;
+        }
+
+        let answer_value = answer.chars().rev().enumerate()
+            .map(|(i, c)| (value_map[&c] as u64) * 10_u64.pow(i as u32))
+            .sum::<u64>();
+
+        let mut sum_value: u64 = 0;
+        for sum_component in &sum {
+            // no leading zeroes
+            if value_map[&sum_component.chars().nth(0).unwrap()] == 0 {
+                continue;
+            }
+            sum_value += sum_component.chars().rev().enumerate()
+                .map(|(i, c)| (value_map[&c] as u64) * 10_u64.pow(i as u32))
+                .sum::<u64>();
+        }
+
+        if sum_value == answer_value {
+            return Some(value_map);
+        }
+    }
+
+    println!("4");
+    None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,4 @@
-// use std::io;
-
-// use crate::prime_factors::factors;
-// use armstrong::is_armstrong_number;
-// use nth_prime::nth_prime::nth;
-// use raindrops::raindrops;
-// use reverse::reverse;
-
-// pub mod armstrong;
-// pub mod nth_prime;
-// pub mod prime_factors;
-// pub mod sum_of_multiples;
-// pub mod raindrops;
-// pub mod reverse;
-
-// pub mod luhn;
-// use palindrome::*;
-// pub mod palindrome;
-
-// pub mod perfect_numbers;
-// use perfect_numbers::*;
-
-// pub mod acronym;
-// use acronym::*;
-
+pub mod alphametics;
 
 fn main() {
     // let mut input = String::new();
@@ -40,32 +16,6 @@ fn main() {
     //     }
     // };
     //
-    // is_armstrong_number(input as u32);
 
-   // nth(input as u32);
-
-    // println!("{:?}", factors(input as u64));
-
-    // println!("{}", sum_of_multiples::sum_of_multiples(4, &[3, 0]));
-
-    // print!("{}", difference(input as u64));
-
-    // println!("{}", raindrops(input as u32));
-
-    // let digits: Vec<char> = vec!['1', '2', '3'];
-    // println!("{:?}", digits[1..0]);
-
-    // let code = "8569 6195 0383 3437";
-    // println!("{}", luhn::is_valid(code));
-
-    // println!("{:?}", largest_series_product::lsp("", 3));
-
-    // Palindrome::new(123456);
-
-    // palindrome_products(1, 99);
-
-    // println!("{:?}", perfect_numbers::classify(2));
-
-    // abbreviate("Portable Network Graphics");
-
+    alphametics::solve("HE + SEES + THE == LIGHT");
 }
