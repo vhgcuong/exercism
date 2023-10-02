1ab41f28cf6e0d202be6a4d755f7cae349f7f20e
Yotoko <44945530+vhgcuong@users.noreply.github.com>
2023-10-02 16:23:18 +0700

    RNA

--- src/rna_transcription.rs
+++ src/rna_transcription.rs
@@ -1,21 +1,47 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+pub struct Dna(String);
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+pub struct Rna(String);
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        todo!("Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        let nucleotides: Vec<char> = vec!['G', 'C', 'T', 'A'];
+        if dna.chars().all(|ch| nucleotides.contains(&ch)) {
+            return Ok(Dna(dna.to_string()));
+        }
+
+        if let Some(position) = dna.chars().position(|ch| !nucleotides.contains(&ch)) {
+            return Err(position);
+        }
+
+        Err(0)
     }
 
     pub fn into_rna(self) -> Rna {
-        todo!("Transform Dna {self:?} into corresponding Rna");
+        let rna = self.0.chars().map(|ch| match ch {
+            'G' => 'C',
+            'C' => 'G',
+            'T' => 'A',
+            'A' => 'U',
+            _ => '_'
+        }).collect::<String>();
+
+        Rna(rna)
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        todo!("Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        let nucleotides: Vec<char> = vec!['G', 'C', 'U', 'A'];
+        if rna.chars().all(|ch| nucleotides.contains(&ch)) {
+            return Ok(Rna(rna.to_string()));
+        }
+
+        if let Some(position) = rna.chars().position(|ch| !nucleotides.contains(&ch)) {
+            return Err(position);
+        }
+
+        Err(0)
     }
 }
