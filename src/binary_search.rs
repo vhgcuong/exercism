d4c0227ee8d032785661795578479c4a6d8d586f
Yotoko <44945530+vhgcuong@users.noreply.github.com>
2023-10-03 14:50:38 +0700

    Binary_search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,5 +1,43 @@
+///
+///
+/// # Arguments
+///
+/// * `array`:
+/// * `key`:
+///
+/// returns: Option<usize>
+///
+/// # Examples
+///
+/// ```
+///
+/// ```
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+    if array.is_empty() {
+        return None;
+    }
+
+    let mut low = 0;
+    let mut hight = array.len() - 1;
+
+    while low <= hight {
+        let middle = (low + hight) / 2;
+        if key == array[middle] {
+            return Some(middle);
+        }
+
+        if key < array[middle] {
+            if middle == 0 {
+                return None;
+            }
+            hight = middle - 1;
+        }
+
+        if key > array[middle] {
+            low = middle + 1;
+        }
+    }
+
+    None
 }
