62aa131d03d196dc46b7bb29e5cbc0c41057377d
Yotoko <vhgcuong95@gmail.com>
2023-10-13 11:00:57 +0700

    Update allergies

--- src/allergies.rs
+++ src/allergies.rs
@@ -1,38 +1,54 @@
-pub struct Allergies(u32);
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum Allergen {
-    Eggs = 1 << 0,
-    Peanuts = 1 << 1,
-    Shellfish = 1 << 2,
-    Strawberries = 1 << 3,
-    Tomatoes = 1 << 4,
-    Chocolate = 1 << 5,
-    Pollen = 1 << 6,
-    Cats = 1 << 7,
+pub struct Allergies {
+    allergens: Vec<Allergen>,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+use self::Allergen::*;
 const ALLERGENS: [Allergen; 8] = [
-        Allergen::Eggs,
-        Allergen::Peanuts,
-        Allergen::Shellfish,
-        Allergen::Strawberries,
-        Allergen::Tomatoes,
-        Allergen::Chocolate,
-        Allergen::Pollen,
-        Allergen::Cats
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
 ];
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        Allergies(score)
+        Self {
+            allergens: Self::calculate_allergens(score),
+        }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        self.0 & *allergen as u32 != 0
+        self.allergens.iter().any(|allergy| allergy == allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        ALLERGENS.iter().filter(|allergen| self.is_allergic_to(allergen)).cloned().collect()
+        self.allergens.clone()
+    }
+
+    fn calculate_allergens(score: u32) -> Vec<Allergen> {
+        let mut allergens = Vec::<Allergen>::new();
+
+        for flag in 0..=7 {
+            if score & 1 << flag != 0 {
+                allergens.push(ALLERGENS[flag as usize]);
+            }
+        }
+        allergens
     }
 }
