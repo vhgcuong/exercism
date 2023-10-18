88683b36d37711b287f3590c5db738bf5429f847
Yotoko <vhgcuong95@gmail.com>
2023-10-18 22:15:16 +0700

    Nucleotide count

--- src/nucleotide_count.rs
+++ src/nucleotide_count.rs
@@ -1,9 +1,36 @@
 use std::collections::HashMap;
 
+const DNA: [char; 4] = ['G', 'C', 'T', 'A'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    todo!("How much of nucleotide type '{nucleotide}' is contained inside DNA string '{dna}'?");
+    if !DNA.contains(&nucleotide) {
+        return Err(nucleotide);
+    }
+
+    let mut count = 0;
+    for ch in dna.chars() {
+        if !DNA.contains(&ch) {
+            return Err(ch);
+        }
+
+        if ch == nucleotide {
+            count += 1;
+        }
+    }
+
+    Ok(count)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    todo!("How much of every nucleotide type is contained inside DNA string '{dna}'?");
+    let mut result: HashMap<char, usize> = HashMap::from([('A', 0), ('T', 0), ('C', 0), ('G', 0)]);
+
+    for ch in dna.chars() {
+        if !DNA.contains(&ch) {
+            return Err(ch);
+        }
+
+        *result.entry(ch).or_default() += 1;
+    }
+
+    Ok(result)
 }
