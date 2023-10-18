5b9c5509d0a134f222d0be22dc87e7531c7a4369
Yotoko <vhgcuong95@gmail.com>
2023-10-18 22:21:26 +0700

    Update nucleotide counts

--- src/nucleotide_count.rs
+++ src/nucleotide_count.rs
@@ -1,36 +1,16 @@
 use std::collections::HashMap;
 
-const DNA: [char; 4] = ['G', 'C', 'T', 'A'];
-
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    if !DNA.contains(&nucleotide) {
-        return Err(nucleotide);
-    }
-
-    let mut count = 0;
-    for ch in dna.chars() {
-        if !DNA.contains(&ch) {
-            return Err(ch);
-        }
-
-        if ch == nucleotide {
-            count += 1;
-        }
-    }
-
-    Ok(count)
+    let mut counts = nucleotide_counts(dna)?;
+    counts.remove(&nucleotide).ok_or(nucleotide)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut result: HashMap<char, usize> = HashMap::from([('A', 0), ('T', 0), ('C', 0), ('G', 0)]);
+    let mut counts: HashMap<char, usize> = ['A', 'C', 'G', 'T'].iter().map(|n| (*n, 0)).collect();
 
-    for ch in dna.chars() {
-        if !DNA.contains(&ch) {
-            return Err(ch);
-        }
-
-        *result.entry(ch).or_default() += 1;
+    for c in dna.chars() {
+        counts.get_mut(&c).map(|count| *count += 1).ok_or(c)?
     }
 
-    Ok(result)
+    Ok(counts)
 }
