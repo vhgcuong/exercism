0e9f4bfb0b16083f7c85a6a04e5616b8c9f8c20e
Yotoko <vhgcuong95@gmail.com>
2023-10-24 11:44:33 +0700

    Update isbn

--- src/isbn_verifier.rs
+++ src/isbn_verifier.rs
@@ -1,29 +1,12 @@
-/// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    if isbn.is_empty() {
-        return false;
+    let nums = isbn.chars().filter(|&c| c.is_numeric() || c == 'X')
+        .collect::<Vec<char>>();
+    match nums.len() {
+        10 => nums.into_iter().rev().enumerate().map(|(i, c)|
+            match c {
+                'X' => 10,
+                _ => c.to_digit(10).unwrap() * (i as u32 + 1),
+            }).sum::<u32>() % 11 == 0,
+        _ => false
     }
-
-    let isbn_digit: Vec<char> = isbn.to_string().chars().filter(|&c| c != '-').collect();
-    if isbn_digit.len() != 10 {
-        return false;
-    }
-
-    let mut sum_isbn: u32 = 0;
-    for (key, value) in isbn_digit.iter().rev().enumerate() {
-        if key == 0 {
-            match value {
-                'X' => sum_isbn += 10,
-                '0'..='9' => sum_isbn += value.to_digit(10).unwrap(),
-                _ => return false
-            }
-        } else {
-            if !value.is_digit(10) {
-                return false;
-            }
-            sum_isbn += (key as u32 + 1) * value.to_digit(10).unwrap();
-        }
-    };
-
-    sum_isbn % 11 == 0
 }
