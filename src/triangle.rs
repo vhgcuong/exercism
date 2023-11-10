aa05cc66631f0f5ba9ab13f9f1a7fc700a5869e1
Yotoko <vhgcuong95@gmail.com>
2023-11-10 15:23:40 +0700

    Generics Triangle

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,12 +1,16 @@
-pub struct Triangle(u64, u64, u64);
+use std::ops::{Add, Sub};
+pub struct Triangle<T>(T, T, T);
 
-impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        if sides.iter().any(|&item| item == 0) ||
-            sides[0] + sides[1] < sides[2] ||
-            sides[0] + sides[2] < sides[1] ||
-            sides[1] + sides[2] < sides[0]
-        {
+impl<T> Triangle<T>
+    where T: Default + Copy + PartialEq + PartialOrd + Add<Output=T> + Sub<Output=T> {
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        if sides.iter().any(|x| *x == T::default()) {
+            return None;
+        }
+
+        let mut sorted_sides = sides;
+        sorted_sides.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        if sorted_sides[2] - (sorted_sides[1] + sorted_sides[0]) > T::default() {
             return None;
         }
 
