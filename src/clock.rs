ce1fead405e3a142abc06b82ccc10f0faab6a627
Cuong.VuHung <vhgcuong95@gmail.com>
2023-11-17 17:31:28 +0700

    Update clock

--- src/clock.rs
+++ src/clock.rs
@@ -9,54 +9,14 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut time: Clock = Clock {
-            hours,
-            minutes
-        };
-
-        time.adjust_minutes();
-        time.adjust_hours();
-
-        time
+        Clock {
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Clock::new(self.hours, self.minutes + minutes)
-    }
-
-    fn adjust_hours(&mut self) {
-        match self.hours {
-            i32::MIN..=-1 => {
-                if self.hours % 24 == 0 {
-                    self.hours = 0;
-                } else {
-                    self.hours = self.hours % 24 + 24;
-                }
-            },
-            24..=i32::MAX => {
-                self.hours = self.hours % 24;
-            },
-            _ => {}
-        }
-    }
-
-    fn adjust_minutes(&mut self) {
-        match self.minutes {
-            i32::MIN..=-1 => {
-                if self.minutes % 60 == 0 {
-                    self.hours += self.minutes / 60;
-                    self.minutes = 0;
-                } else {
-                    self.hours += self.minutes / 60 - 1;
-                    self.minutes = self.minutes % 60 + 60;
-                }
-            },
-            60..=i32::MAX => {
-                self.hours += self.minutes / 60;
-                self.minutes = self.minutes % 60;
-            },
-            _ => {}
-        }
     }
 }
 
